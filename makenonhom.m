1af782b5508655a7f2b78df30c6ab6607136396d
Jesus Briales <jesusbriales@gmail.com>
2017-11-08 20:13:04 +0100

    makenonhom works with vector and matrix (horzcat of vert-vector)

--- makenonhom.m
+++ makenonhom.m
@@ -2,12 +2,13 @@ function nonhom_x = makenonhom( x )
 % nonhom_x = makenonhom( x )
 % Returns the homogeneous vector (append 1 at the end of a column vector)
 
-if size(x,2) ~= 1
-  error('Use only column vectors with makenonhom');
-end
-
-if abs(x(end)) < 1e-6
+if any( abs(x(end,:)) < 1e-6 )
   warning('Hom component is zero');
 end
 
-nonhom_x = x(1:end-1) / x(end);
+if isvector(x)
+  nonhom_x = x(1:end-1) / x(end);
+else
+  n = size(x,1) - 1;
+  nonhom_x = x(1:n,:) ./ repmat(x(end,:),n,1);
+end
